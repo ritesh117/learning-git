7753668673b935aebb504fbf41f5c9bb429f3532
ritesh117 <myself002.rr@gmail.com>
2025-12-25 04:16:01 +0000

    Add Flask project files

---
+++ Flask_app/back-end/app.py
@@ -0,0 +1,39 @@
+from flask import Flask, request, jsonify
+from dotenv import load_dotenv
+import os
+import pymongo
+
+load_dotenv()
+
+MONGO_URI = os.getenv("MONGO_URI")
+client = pymongo.MongoClient(MONGO_URI)
+
+db = client.test
+
+collection = db['flask_assingment']
+
+app = Flask(__name__)
+
+
+@app.route('/submit', methods=['GET', 'POST'])
+def submit():
+    form_data = dict(request.json)
+    collection.insert_one(form_data)
+    
+    return 'data submitted successfully!'
+
+@app.route('/view')
+def view():
+    data = collection.find()
+    data = list(data)  # Convert cursor to list for easier manipulation
+    for item in data:
+        print(item)
+        del item['_id']  # Remove the MongoDB ObjectId field for cleaner output
+        data={
+            'data': data,
+        }
+    return jsonify(data)
+    
+if __name__=='__main__':
+
+    app.run(host='0.0.0.0',port=9000,debug=True)
